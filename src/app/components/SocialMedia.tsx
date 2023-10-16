969122610955fb5beae30f84f297c9a3899e1a98
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-10-16 10:45:25 +0300

    icon

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -1,29 +1,33 @@
 import { BsGithub, BsLinkedin, BsFacebook } from "react-icons/bs";
 import { HiMail } from "react-icons/hi";
+import { RiMailSendLine } from "react-icons/ri";
 
 const SocialMedia: any = () => {
   return (
     <div className="flex items-center ml-1 gap-4">
       <a
         href="https://www.facebook.com/divergytis"
-        className="hover:cursor-pointer text-2xl text-teal-400 hover:text-emerald-100">
+        className="hover:cursor-pointer text-2xl text-teal-400 hover:text-emerald-100"
+      >
         <BsFacebook />
       </a>
       <a
         href="https://github.com/LeoGytis"
-        className="hover:cursor-pointer text-2xl text-teal-400 hover:text-emerald-100">
-
+        className="hover:cursor-pointer text-2xl text-teal-400 hover:text-emerald-100"
+      >
         <BsGithub />
       </a>
       <a
         href="https://www.linkedin.com/in/gytis-leonavicius-74839519a/"
-        className="hover:cursor-pointer text-2xl text-teal-400 hover:text-emerald-100">
+        className="hover:cursor-pointer text-2xl text-teal-400 hover:text-emerald-100"
+      >
         <BsLinkedin />
       </a>
       <a
         href="mailto:leogytis@gmail.com"
-        className="hover:cursor-pointer text-3xl text-teal-400 hover:text-emerald-100">
-        <HiMail />
+        className="hover:cursor-pointer text-3xl text-teal-400 hover:text-emerald-100"
+      >
+        <RiMailSendLine />
       </a>
     </div>
   );
