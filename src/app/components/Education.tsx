60e05fd5b20533a2b002f77b21eff634009c29f1
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-10-17 09:06:10 +0300

    hell

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
+import Link from "next/link";
 import ktu from "../../../public/images/ktu_svg.svg";
 import bit from "../../../public/images/bit_svg.svg";
 
@@ -9,7 +10,10 @@ const Education: any = () => {
       id="education"
       className="flex flex-col gap-8 text-lg text-slate-300 lg:p-6 mb-16 md:mb-24 scroll-mt-16 "
     >
-      <div className="flex gap-2">
+      <Link
+        href="https://en.ktu.edu/"
+        className="flex gap-2 hover:text-[#5eead4]"
+      >
         <Image src={ktu} width={40} height={40} alt="ktu" className="mr-2" />
         <div>
           <p>Kaunas University of Technology</p>
@@ -17,15 +21,15 @@ const Education: any = () => {
             Bachelor's degree in Information System Design
           </p>
         </div>
-      </div>
+      </Link>
 
-      <div className="flex gap-2">
+      <Link href="https://bit.lt/" className="flex gap-2 hover:text-[#5eead4]">
         <Image src={bit} width={40} height={40} alt="bit" className="mr-3" />
         <div>
           <p>Baltic Institute of Technology</p>
           <p className="text-sm">Full-stack developer JavaScript + PHP</p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
