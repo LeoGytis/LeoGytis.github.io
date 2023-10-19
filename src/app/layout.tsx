e95505c58614ac345579ae9258fa40ab88e82df1
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-10-19 14:48:08 +0300

    bandom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Ubuntu, Roboto, Nunito, Inter } from "next/font/google";
+import website_image from "../../public/images/website_image.png";
 
 const nunito = Nunito({
   weight: "400",
@@ -8,17 +9,17 @@ const nunito = Nunito({
 });
 
 export const metadata: Metadata = {
-  title: "Gytis Leonavicius",
-  description: "Gytis Leonavicius portfolio page",
-  // openGraph: {
-  //   title: titlePage,
-  //   description: descriptionPage,
-  //   images: [
-  //     {
-  //       url: image.src,
-  //     },
-  //   ],
-  // },
+  title: "Gytis Leonavicius Web Developer",
+  description: "Inclusive products and digital experiences for the web.",
+  openGraph: {
+    title: "Gytis Leonavicius Web Developer",
+    description: "Inclusive products and digital experiences for the web.",
+    images: [
+      {
+        url: website_image.src,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
