9c26d3724ea817af8875dee3a871618e3b17e709
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-10-19 14:44:25 +0300

    save and deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,21 +10,12 @@ const nunito = Nunito({
 export const metadata: Metadata = {
   title: "Gytis Leonavicius",
   description: "Gytis Leonavicius portfolio page",
-  openGraph: {
-    images: "/images/website_image.png",
-  },
   // openGraph: {
+  //   title: titlePage,
+  //   description: descriptionPage,
   //   images: [
   //     {
-  //       url: "https://nextjs.org/og.png",
-  //       width: 800,
-  //       height: 600,
-  //     },
-  //     {
-  //       url: "https://nextjs.org/og-alt.png",
-  //       width: 1800,
-  //       height: 1600,
-  //       alt: "My custom alt",
+  //       url: image.src,
   //     },
   //   ],
   // },
