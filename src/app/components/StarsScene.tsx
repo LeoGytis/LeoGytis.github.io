f305a5f34b714efc7ba03ea9613750f8ba91145e
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-10-29 21:33:02 +0200

    falling stars in progress

--- src/app/components/StarsScene.tsx
+++ src/app/components/StarsScene.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const ThreeScene: React.FC = () => {
+const StarsScene = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -15,48 +15,57 @@ const ThreeScene: React.FC = () => {
       );
       const renderer = new THREE.WebGLRenderer();
 
-      //   const renderer = new THREE.WebGLRenderer({
-      //     canvas: document.querySelector("#bg"),
-      //   });
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(window.innerWidth, window.innerHeight);
       containerRef.current?.appendChild(renderer.domElement);
       camera.position.setZ(50);
       camera.position.setX(50);
 
-      const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-      const material = new THREE.MeshStandardMaterial({
-        color: 0xff6347,
-      });
-      const torus = new THREE.Mesh(geometry, material);
-      //   scene.add(torus);
-
-      const pointLight = new THREE.PointLight(0xffff00, 15, 0, 0);
-      pointLight.position.set(15, 15, 5);
-
       const ambientLight = new THREE.AmbientLight(0xffffff);
-      scene.add(pointLight);
+      scene.add(ambientLight);
 
-      const lightHelper = new THREE.PointLightHelper(pointLight);
       const gridHelper = new THREE.GridHelper(200, 50);
-      //   scene.add(lightHelper, gridHelper);
+      scene.add(gridHelper);
 
       const controls = new OrbitControls(camera, renderer.domElement);
 
-      const addStar = () => {
-        const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-        const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+      const fallingStar = () => {
+        const geometry = new THREE.BoxGeometry(0.01, 5, 0.01);
+        const material = new THREE.MeshBasicMaterial({
+          transparent: true,
+          opacity: 0.8,
+          color: 0x5eead4,
+        });
         const star = new THREE.Mesh(geometry, material);
 
-        const [x, y, z] = Array(3)
-          .fill(null)
-          .map(() => THREE.MathUtils.randFloatSpread(100));
+        const x = THREE.MathUtils.randFloatSpread(100);
+        const y = THREE.MathUtils.randFloat(50, 50); // Random Y-coordinate within a range
+        const z = THREE.MathUtils.randFloatSpread(100);
 
         star.position.set(x, y, z);
         scene.add(star);
+
+        // Function to update the star's position in the animation loop
+        const updateStarPosition = () => {
+          star.position.y -= 0.2; // Adjust the falling speed
+          if (star.position.y < -10) {
+            // If the star falls below a certain Y-coordinate, remove it
+            scene.remove(star);
+          }
+        };
+
+        // Add the update function to the animation loop
+        const animate = () => {
+          updateStarPosition();
+          renderer.render(scene, camera);
+          requestAnimationFrame(animate);
+        };
+
+        animate(); // Start the animation loop
       };
 
-      Array(200).fill(null).forEach(addStar);
+      // Call fallingStar to add many falling stars
+      Array(200).fill(null).forEach(fallingStar);
 
       // Scroll Animation
 
@@ -73,8 +82,6 @@ const ThreeScene: React.FC = () => {
 
       // Render the scene and camera
       const renderScene = () => {
-        // torus.rotation.x += 0.01;
-        // torus.rotation.y += 0.01;
         renderer.render(scene, camera);
         controls.update();
         requestAnimationFrame(renderScene);
@@ -84,4 +91,4 @@ const ThreeScene: React.FC = () => {
   }, []);
   return <div ref={containerRef} />;
 };
-export default ThreeScene;
+export default StarsScene;
