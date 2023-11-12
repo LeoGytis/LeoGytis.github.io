96141aeacf82ec64d163f64758e6a7f9afae89b4
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-11-12 15:44:17 +0200

    ok to save

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -10,16 +10,16 @@ const Education: any = () => {
   return (
     <div
       id="education"
-      className="flex flex-col gap-8 text-lg lg:p-6 mb-16 md:mb-24 scroll-mt-16 "
+      className="flex flex-col gap-8 text-lg lg:p-6 mb-16 md:mb-24 scroll-mt-16 experience_container"
     >
       <Link
         href="https://en.ktu.edu/"
         className="flex gap-2 hover:text-secondary"
       >
         <SvgKtu width={40} height={40} className="fill-tag mr-3" />
-        <div>
-          <p>Kaunas University of Technology</p>
-          <p className="text-sm">
+        <div className="hover:text-secondary">
+          <div>Kaunas University of Technology</div>
+          <p className="text-sm ">
             Bachelor's degree in Information System Design
           </p>
         </div>
@@ -27,8 +27,8 @@ const Education: any = () => {
 
       <Link href="https://bit.lt/" className="flex gap-2 hover:text-secondary">
         <SvgBit width={40} height={40} className="fill-tag mr-3" />
-        <div>
-          <p>Baltic Institute of Technology</p>
+        <div className="!important:hover:text-secondary">
+          <div>Baltic Institute of Technology</div>
           <p className="text-sm">Full-stack developer JavaScript + PHP</p>
         </div>
       </Link>
