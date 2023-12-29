0508d3b70c8c5e4feb701e7d910bc2cfd4ab63fb
leogytis <leogytis@gmail.com>
2023-12-29 11:34:38 +0200

    back to file rename

---
+++ src/app/utils/ToolTags.tsx
@@ -0,0 +1,10 @@
+const ToolTags: any = () => {
+  return (
+    <div
+      id="about"
+      className="flex flex-col gap-4 text-lg lg:p-6 lg:pt-0 mb-16 md:mb-20"
+    ></div>
+  );
+};
+
+export default ToolTags;
