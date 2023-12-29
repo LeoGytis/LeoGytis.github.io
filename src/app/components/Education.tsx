e7e2b6b6c84745b394f03a4f5c537453c25b8773
leogytis <leogytis@gmail.com>
2023-12-29 22:57:51 +0200

    group tw class works

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -1,8 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import Image from "next/image";
 import Link from "next/link";
-import ktu from "../../../public/images/ktu_svg.svg";
-import bit from "../../../public/images/bit_svg.svg";
 import SvgKtu from "../../../public/images/ktu_svg2";
 import SvgBit from "../../../public/images/bit_svg2";
 
@@ -14,22 +11,24 @@ const Education: any = () => {
     >
       <Link
         href="https://en.ktu.edu/"
-        className="flex gap-2 hover:text-secondary"
+        className="flex items-center gap-2 group"
       >
         <SvgKtu width={40} height={40} className="fill-tag mr-3" />
-        <div className="hover:text-secondary">
+        <div className="group-hover:text-secondary">
           <div>Kaunas University of Technology</div>
-          <p className="text-sm ">
+          <p className="text-sm group-hover:text-secondary">
             Bachelor's degree in Information System Design
           </p>
         </div>
       </Link>
 
-      <Link href="https://bit.lt/" className="flex gap-2 hover:text-secondary">
+      <Link href="https://bit.lt/" className="flex items-center gap-2 group">
         <SvgBit width={40} height={40} className="fill-tag mr-3" />
-        <div className="!important:hover:text-secondary">
+        <div className="group-hover:text-secondary">
           <div>Baltic Institute of Technology</div>
-          <p className="text-sm">Full-stack developer JavaScript + PHP</p>
+          <p className="text-sm group-hover:text-secondary">
+            Full-stack developer JavaScript + PHP
+          </p>
         </div>
       </Link>
     </div>
