1adc8343bb8714a5799767cd675c40e501913612
leogytis <leogytis@gmail.com>
2023-12-30 13:12:01 +0200

    save

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -20,24 +20,22 @@ const Project = ({
   tags,
 }: ProjectProps) => {
   return (
-    <div className="mb-12 hover_border">
-      <div className="lg:p-6">
-        <div className="flex gap-6">
-          <div className="overflow-hidden rounded cursor-pointer">
-            <Image
-              src={imageSrc}
-              width={300}
-              height={300}
-              alt={title}
-              className="rounded hover:scale-125 ease-out duration-1000 transform"
-            />
-          </div>
-          <div className="flex flex-col gap-3 w-2/3">
-            <p>{description}</p>
-            <a href={link}>Project link</a>
-            <div className="flex flex-wrap gap-2">
-              <ToolTags tags={tags} />
-            </div>
+    <div className="mb-12 lg:p-6 hover_border">
+      <div className="flex flex-col sm:flex-row gap-6 w-full">
+        <div className="overflow-hidden rounded cursor-pointer">
+          <Image
+            src={imageSrc}
+            width={300}
+            height={300}
+            alt={title}
+            className="rounded hover:scale-125 ease-out duration-1000 transform"
+          />
+        </div>
+        <div className="flex flex-col gap-3 sm:w-2/3">
+          <p>{description}</p>
+          <a href={link}>Project link</a>
+          <div className="flex flex-wrap gap-2">
+            <ToolTags tags={tags} />
           </div>
         </div>
       </div>
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -12,26 +12,30 @@ const Projecs: any = () => {
   return (
     <div id="projects">
       <Project
-        title="E-Study"
-        imageSrc={study}
-        link="https://leogytis.github.io/e-study/"
-        description="E-Study is a learning-oriented landing page that showcases my use of various React libraries. It's designed to provide a practical demonstration of React-based web development for educational purposes."
-        tags={["CSS", "Styled Components", "TypeScript", "React"]}
-      />
-
-      <Project
         title="Sris"
         imageSrc={sris}
         link="https://sris.biip.lt/"
         description="An information system for protected species provided by the State Protected Areas Service under the Ministry of Environment, offering data on the protected plant, animal, and fungal species in the Republic of Lithuania, along with their habitats and locations."
         tags={[
-          "CSS",
-          "Styled Components",
-          "TypeScript",
           "React",
-          "Redux",
-          "React Router",
+          "TypeScript",
           "Formik",
+          "Redux",
+          "React-Router",
+          "StyledComponents",
+        ]}
+      />
+      <Project
+        title="E-Study"
+        imageSrc={study}
+        link="https://leogytis.github.io/e-study/"
+        description="E-Study is a learning-oriented landing page that showcases my use of various React libraries. It's designed to provide a practical demonstration of React-based web development for educational purposes."
+        tags={[
+          "React",
+          "TypeScript",
+          "Styled-Components",
+          "React-Icons",
+          "React-Scroll",
         ]}
       />
 
@@ -48,7 +52,7 @@ const Projecs: any = () => {
         imageSrc={brofinity}
         link="https://brofinity.com/"
         description="Developed fashion clothing e-commerce site on Shopify, leveraging Liquid to deliver a seamless shopping platform for our collection."
-        tags={["CSS", "Tailwind", "JavaScript", "Shopify", "Liquid"]}
+        tags={["Shopify", "Liquid", "JavaScript", "Tailwind"]}
       />
 
       <Project
@@ -56,7 +60,7 @@ const Projecs: any = () => {
         imageSrc={pz}
         link="https://pienozvaigzdes.lt/business-solutions-and-ingredients/"
         description="Developed a dynamic B2B products page for seamless access to our offerings by potential business clients. The page prioritizes clarity and ease of navigation, ensuring a smooth user experience."
-        tags={["CSS", "Tailwind", "TypeScript", "React", "Next.js"]}
+        tags={["Next.js", "React", "TypeScript", "Tailwind", "CSS"]}
       />
 
       <Project
@@ -64,7 +68,7 @@ const Projecs: any = () => {
         imageSrc={gridster}
         link="https://leogytis.github.io/gridster/"
         description="UI project that involves creating a resizable grid interface. Users can set the grid dimensions, and the application generates 'start' and 'end' points. Users can interactively mark squares as 'clear' or 'filled'. The project also includes a pathfinding algorithm to highlight the shortest path from 'start' to 'end' on the grid, showcasing my UI development and third-party library integration skills."
-        tags={["CSS", "StyledComponents", "TypeScript", "React"]}
+        tags={["React", "TypeScript", "Styled-Components", "CSS"]}
       />
 
       <Project
@@ -72,7 +76,7 @@ const Projecs: any = () => {
         imageSrc={ip_royal}
         link="https://leogytis.github.io/ip_royal/"
         description="Exploring Vue.js for the first time, I created a dynamic dashboard using Tailwind CSS and TypeScript. This project tested my proficiency in a new framework and assessed Tailwind's efficiency. The interactive dashboard, with real-time updates, showcased a seamless integration of Tailwind CSS, TypeScript, and Vue.js, demonstrating adaptability to new technologies."
-        tags={["CSS", "Tailwind", "TypeScript", "Vue.js"]}
+        tags={["Vue.js", "TypeScript", "Tailwind", "CSS"]}
       />
     </div>
   );
