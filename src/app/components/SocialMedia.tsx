f267439ee0f7a28540b9cfc2b3b4a1b071348988
leogytis <leogytis@gmail.com>
2023-12-31 11:02:42 +0200

    save for now

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -1,38 +1,22 @@
-import { BsGithub, BsLinkedin, BsFacebook } from "react-icons/bs";
-import { HiMail } from "react-icons/hi";
-import { RiMailSendLine } from "react-icons/ri";
-import { TbMailFilled } from "react-icons/tb";
-import { SiGmail } from "react-icons/si";
-import { BiLogoGmail } from "react-icons/bi";
+import { BsGithub, BsFacebook } from "react-icons/bs";
 import SvgGmail from "../../../public/images/svg/svgGmail";
 import SvgLinkedIn from "../../../public/images/svg/SvgLinkedIn";
 
 const SocialMedia: any = () => {
   return (
     <div className="flex items-center ml-1 gap-4">
-      <a
-        href="https://www.facebook.com/divergytis"
-        className="hover:cursor-pointer text-2xl text-tag hover:text-nav-inactive"
-      >
-        <BsFacebook className="hover:text-primary" />
+      <a href="https://www.facebook.com/divergytis" className=" ">
+        <BsFacebook className="text-2xl text-tag hover:cursor-pointer hover:text-primary" />
       </a>
-      <a
-        href="https://github.com/LeoGytis"
-        className="hover:cursor-pointer text-2xl text-tag hover:text-nav-inactive"
-      >
-        <BsGithub className="hover:text-primary" />
+      <a href="https://github.com/LeoGytis">
+        <BsGithub className="text-2xl text-tag hover:cursor-pointer hover:text-primary" />
       </a>
-      <a
-        href="https://www.linkedin.com/in/gytis-leonavicius-74839519a/"
-        className="hover:cursor-pointer text-[1.7rem] text-tag hover:text-nav-inactive"
-      >
-        <SvgLinkedIn className="fill-tag hover:fill-primary" />
+
+      <a href="https://www.linkedin.com/in/gytis-leonavicius-74839519a/">
+        <SvgLinkedIn className="w-12 text-[1.7rem] fill-tag  hover:cursor-pointer hover:fill-primary" />
       </a>
-      <a
-        href="mailto:leogytis@gmail.com"
-        className="hover:cursor-pointer text-2xl text-tag hover:text-nav-inactive"
-      >
-        <SvgGmail className="fill-tag hover:fill-primary" />
+      <a href="mailto:leogytis@gmail.com">
+        <SvgGmail className=" text-2xl fill-tag hover:cursor-pointer hover:fill-primary" />
       </a>
     </div>
   );
