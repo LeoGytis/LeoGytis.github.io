2edfc9692a7b3a18b6cf2b736610d939c0888fa4
leogytis <leogytis@gmail.com>
2024-01-25 07:59:51 +0200

    another try

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-
+	output: "export",
+	images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withStaticImport, withBundleAnalyzer, withCustomWebpack]
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...defaultConfig, ...config })
+}
