6767d3e41354d2f35afaf461381c1626dcbbe4e3
leogytis <leogytis@gmail.com>
2024-01-25 08:27:08 +0200

    more try

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -1,6 +1,6 @@
 import { BsGithub, BsFacebook } from "react-icons/bs";
 import SvgGmail from "../../../public/images/svg/svgGmail";
-import SvgLinkedIn from "../../../public/images/svg/SvgLinkedIn";
+// import SvgLinkedIn from "../../../public/images/svg/SvgLinkedIn";
 
 const SocialMedia: any = () => {
   return (
@@ -13,7 +13,7 @@ const SocialMedia: any = () => {
       </a>
 
       <a href="https://www.linkedin.com/in/gytis-leonavicius-74839519a/">
-        <SvgLinkedIn className="w-7 text-[1.7rem] fill-tag  hover:cursor-pointer hover:fill-primary" />
+        {/* <SvgLinkedIn className="w-7 text-[1.7rem] fill-tag  hover:cursor-pointer hover:fill-primary" /> */}
       </a>
       <a href="mailto:leogytis@gmail.com">
         <SvgGmail className="text-2xl fill-tag hover:cursor-pointer hover:fill-primary" />
