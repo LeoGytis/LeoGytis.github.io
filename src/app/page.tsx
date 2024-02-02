8831826a57338fc7a47574298cba6acb50a9c177
leogytis <leogytis@gmail.com>
2024-02-02 08:25:10 +0200

    commit again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,24 @@ import ThemeSwitch from "../app/utils/ThemeSwitch";
 import StarsScene from "./components/StarsScene";
 
 export default function Home() {
-  return (
-    <div className="mx-auto max-w-screen-xl p-8 md:p-12 lg:p-20 lg:py-12">
-      <div className="fixed top-0 left-0">
-        <StarsScene />
-      </div>
-      <div className="flex flex-col lg:flex-row gap-10">
-        <header className="lg:w-1/3">
-          <NavBar />
-        </header>
-        <main className="lg:w-2/3 lg:pl-12 relative">
-          <div className="hidden lg:inline-block falling_line"></div>
-          <ThemeSwitch />
-          <About />
-          <Education />
-          <Experience />
-          <Projecs />
-        </main>
-      </div>
-    </div>
-  );
+	return (
+		<div className="mx-auto max-w-screen-xl p-8 md:p-12 lg:p-20 lg:py-12">
+			<div className="fixed top-0 left-0">
+				<StarsScene />
+			</div>
+			<div className="flex flex-col lg:flex-row gap-10">
+				<header className="lg:w-1/3">
+					<NavBar />
+				</header>
+				<main className="lg:w-2/3 lg:pl-12 relative blur-animation">
+					<span className="hidden lg:inline-block falling_line"></span>
+					<ThemeSwitch />
+					<About />
+					<Education />
+					<Experience />
+					<Projecs />
+				</main>
+			</div>
+		</div>
+	);
 }
