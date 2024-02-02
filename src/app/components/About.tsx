ae9a9113bd750e84bc1ef2428394a3c5a805429b
leogytis <leogytis@gmail.com>
2024-02-02 08:24:48 +0200

    animation fixes

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,28 +1,26 @@
 const About: any = () => {
 	return (
-		<div className="blur-animation">
-			<div
-				id="about"
-				className="flex flex-col gap-4 text-lg lg:p-6 mb-16 md:mb-20 hover_border"
-			>
-				<p>
-					I have experience developing web applications, from simple
-					landing pages to progressive web applications. I strive to
-					create software that not only functions efficiently under
-					the hood, but also provides intuitive, pixel-perfect user
-					experiences.
-				</p>
-				<p>
-					I love learning new and better ways to create seamless user
-					experiences with clean, efficient and scalable code. I
-					consider work an ongoing education, and I am always looking
-					for opportunities to work with those who are willing to
-					share their knowledge as much as I want to learn. At the end
-					of the day, my primary goal is to create something beautiful
-					with people that bring out the best in me.
-				</p>
-				<p>At my free time I am out in the woods or swimming.</p>
-			</div>
+		<div
+			id="about"
+			className="flex flex-col gap-4 text-lg lg:p-6 mb-16 md:mb-20 hover_border"
+		>
+			<p>
+				I have experience developing web applications, from simple
+				landing pages to progressive web applications. I strive to
+				create software that not only functions efficiently under the
+				hood, but also provides intuitive, pixel-perfect user
+				experiences.
+			</p>
+			<p>
+				I love learning new and better ways to create seamless user
+				experiences with clean, efficient and scalable code. I consider
+				work an ongoing education, and I am always looking for
+				opportunities to work with those who are willing to share their
+				knowledge as much as I want to learn. At the end of the day, my
+				primary goal is to create something beautiful with people that
+				bring out the best in me.
+			</p>
+			<p>At my free time I am out in the woods or swimming.</p>
 		</div>
 	);
 };
