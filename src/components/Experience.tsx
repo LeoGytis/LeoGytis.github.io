dafe1160919ccb42d4e04e2a85a4502c6269ffdf
leogytis <leogytis@gmail.com>
2024-02-28 16:34:55 +0200

    no hover more

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -13,14 +13,14 @@ const Experience: any = () => {
 					<div className="flex gap-4">
 						<Image src={elnis} alt="elnis" className="w-20 h-20 rounded" />
 						<div>
-							<Link href="https://telesoftas.com/" className="hover:text-secondary">
-								<h2 className="inline-block text-lg hover:bg-transparent">Elnis</h2>
+							<Link href="https://telesoftas.com/" className="inline-block text-xl hover:text-secondary">
+								Elnis
 							</Link>
-							<h1 className="font-semibold text-xl hover:bg-transparent">Web Developer</h1>
-							<h5 className="text-xs mb-4 hover:bg-transparent">2023 October - present</h5>
+							<h2 className="text-lg">Web Developer</h2>
+							<h5 className="text-xs mb-4">2023 October - present</h5>
 						</div>
 					</div>
-					<p className="mb-4 hover:bg-transparent">
+					<p className="mb-4">
 						Currently working on e-commerce projects, specializing in diverse product categories, I am
 						responsible for crafting visually engaging and responsive web interfaces. In this role, I design
 						user-friendly product pages and ensure cross-browser compatibility to enhance the overall online
@@ -34,14 +34,14 @@ const Experience: any = () => {
 					<div className="flex gap-4">
 						<Image src={telesoftas} alt="elnis" className="w-20 h-20 rounded" />
 						<div>
-							<h1 className="font-semibold text-xl hover:bg-transparent">Web Developer</h1>
-							<Link href="https://telesoftas.com/" className="hover:text-secondary">
-								<h2 className="inline-block text-lg hover:bg-transparent">TeleSoftas</h2>
+							<Link href="https://telesoftas.com/" className="inline-block text-xl hover:text-secondary">
+								TeleSoftas
 							</Link>
-							<h5 className="text-xs mb-4 hover:bg-transparent">2023 June - September</h5>
+							<h2 className="text-lg">Web Developer</h2>
+							<h5 className="text-xs mb-4">2023 June - September</h5>
 						</div>
 					</div>
-					<p className="mb-4 hover:bg-transparent">
+					<p className="mb-4">
 						Working with an e-commerce projects specializing in products and fashion, I was responsible for
 						creating visually engaging and responsive web interfaces. This role involved designing
 						user-friendly product pages, optimizing site performance, and ensuring cross-browser
@@ -67,24 +67,21 @@ const Experience: any = () => {
 					<div className="flex gap-4">
 						<Image src={aplinkos_ministerija} alt="elnis" className="w-20 h-20 rounded" />
 						<div className="">
-							<h1 className="font-semibold text-xl hover:bg-transparent">Front-end Developer</h1>
-							<Link href="https://telesoftas.com/" className="hover:text-secondary">
-								<h2 className="inline-block text-lg hover:bg-transparent">
-									Ministry of Environment of the Republic of Lithuania
-								</h2>
+							<Link href="https://telesoftas.com/" className="inline-block text-xl hover:text-secondary">
+								Ministry of Environment of the Republic of Lithuania
 							</Link>
-
-							<h5 className="text-xs mb-4 hover:bg-transparent">October 2022 - June 2023</h5>
+							<h2 className="font-medium text-xl">Front-end Developer</h2>
+							<h5 className="text-xs mb-4">October 2022 - June 2023</h5>
 						</div>
 					</div>
-					<p className="mb-4 hover:bg-transparent">
+					<p className="mb-4">
 						In my role as a Frontend Developer at the BIIP project, I contributed to the development of a
 						user orieanted web application, which focused on helping nature supervisors collect and review
 						data related to the preservation of nature and the study of invasive and endangered species.
 						This work was both intriguing and deeply rewarding, as it allowed me to play a meaningful role
 						in making a positive impact on our environment.
 					</p>
-					<div className="flex hover:bg-transparent"></div>
+					<div className="flex"></div>
 
 					<ToolTags
 						tags={[
