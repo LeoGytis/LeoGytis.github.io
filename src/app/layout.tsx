52c9f4cf9e5b8b0b6c32be819ed4b1ae15ca7a91
leogytis <leogytis@gmail.com>
2024-05-23 06:51:11 +0300

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,33 @@
 import "./globals.css";
-import type { Metadata } from "next";
-import { Nunito, Inter, Lato, Quicksand } from "next/font/google";
+import type {Metadata} from "next";
+import {Nunito, Inter, Lato, Quicksand} from "next/font/google";
 import website_image from "../../public/images/website_image.png";
 
 const inter = Lato({
-  weight: "400",
-  subsets: ["latin"],
+	weight: "400",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://leogytis.github.io/"),
-  title: "Gytis Leonavicius | Web Developer",
-  description: "Inclusive products and digital experiences for the web.",
-  authors: [{ name: "Gytis Leonavicius", url: "https://leogytis.github.io/" }],
-  openGraph: {
-    title: "Gytis Leonavicius | Web Developer",
-    description: "Inclusive products and digital experiences for the web.",
-    images: [
-      {
-        url: website_image.src,
-      },
-    ],
-  },
+	metadataBase: new URL("https://leogytis.github.io/"),
+	title: "Gytis | Web Developer",
+	description: "Inclusive products and digital experiences for the web.",
+	authors: [{name: "Gytis Leonavicius", url: "https://leogytis.github.io/"}],
+	openGraph: {
+		title: "Gytis | Web Developer",
+		description: "Inclusive products and digital experiences for the web.",
+		// images: [
+		// 	{
+		// 		url: website_image.src,
+		// 	},
+		// ],
+	},
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({children}: {children: React.ReactNode}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link as ScrollLink } from "react-scroll";
+import {Link as ScrollLink} from "react-scroll";
 import SocialMedia from "./SocialMedia";
 
 const NavBar: any = () => {
@@ -7,7 +7,7 @@ const NavBar: any = () => {
 			<div className="blur-animation">
 				<h1 className="text-4xl sm:text-5xl font-bold pb-3">Gytis Leonavičius</h1>
 				<h2 className="text-lg sm:text-xl font-medium pb-4">Web developer</h2>
-				<h3 className="max-w-xs">Inclusive products and digital experiences for the web.</h3>
+				<h3>Inclusive products and digital experiences for the web</h3>
 			</div>
 			<nav className="hidden lg:flex flex-col text-lg text-nav_inactive gap-2 mb-14 blur-animation">
 				<ScrollLink className="nav_item" spy smooth offset={-80} to="about">
