164154ba1ba260b16c029259b9095de01dc70505
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-05-27 16:16:26 +0300

    save

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,7 +2,6 @@ import acertip from '../../public/images/experience/acertip.jpg';
 import acterio from '../../public/images/experience/acterio.jpg';
 import aplinkos_ministerija from '../../public/images/experience/aplinkos_ministerija.jpg';
 import elnis from '../../public/images/experience/elnis.jpg';
-import telesoftas from '../../public/images/experience/telesoftas.png';
 import ExperienceWrapper from './ExperienceWrapper';
 
 const Experience = () => {
@@ -32,20 +31,20 @@ const Experience = () => {
         date="2023 October - 2024 March"
         description="Developing user-friendly e-commerce webpages: Optimizing accessibility and usability across various devices through responsive design principles. Enhancing online transactions and user engagement by integrating advanced features and functionalities."
       />
-      <ExperienceWrapper
+      {/* <ExperienceWrapper
         imageSrc={telesoftas}
         title="TeleSoftas"
         link="https://telesoftas.com/"
         role="Web Developer"
         date="2023 June - September"
         description="Worked on e-commerce projects specializing in products and fashion, creating visually engaging and responsive web interfaces. Designed user-friendly product pages, optimized site performance, and ensured cross-browser compatibility to enhance the overall online shopping experience."
-      />
+      /> */}
       <ExperienceWrapper
         imageSrc={aplinkos_ministerija}
         title="Ministry of Environment of the Republic of Lithuania"
         link="https://am.lrv.lt/lt/"
         role="Front-end Developer"
-        date="2022 October - 2023 June"
+        date="2022 October - 2023 September"
         description="	In my role as a Frontend Developer at the BIIP project, I contributed to the development of a	user orieanted web application, which focused on helping nature supervisors collect and review data related to the preservation of nature and the study of invasive and endangered species. This work was both intriguing and deeply rewarding, as it allowed me to play a meaningful role in making a positive impact on our environment."
       />
     </div>
