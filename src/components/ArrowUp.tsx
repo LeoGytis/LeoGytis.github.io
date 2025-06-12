175469ffb0ea245017b40e6f458bfe94c16589f0
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-06-12 16:29:18 +0300

    save

--- src/components/ArrowUp.tsx
+++ src/components/ArrowUp.tsx
@@ -33,13 +33,13 @@ const ArrowUp = () => {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-8 right-8 p-3 rounded-full border border-secondary text-white transition-all duration-300 hover:shadow-border cursor-pointer group hover:bg-background ${
+      className={`fixed bottom-8 right-8 p-2 rounded-full border border-secondary text-white transition-all duration-300 hover:shadow-border cursor-pointer group hover:bg-background ${
         isVisible
           ? 'opacity-100 translate-y-0'
           : 'opacity-0 translate-y-10 pointer-events-none'
       }`}
     >
-      <RiArrowUpDoubleLine className="size-6 text-secondary group-hover:text-primary" />
+      <RiArrowUpDoubleLine className="w-6 h-6 text-secondary group-hover:text-primary" />
     </button>
   );
 };
