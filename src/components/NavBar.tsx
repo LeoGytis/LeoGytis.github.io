272ff6198921d7fe6c85b0f55094bf220e7e0d1f
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-06-30 11:21:11 +0200

    save

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,14 @@
 import { Link as ScrollLink } from 'react-scroll';
 import SocialMedia from './SocialMedia';
 
-const NavBar: any = () => {
+const NavItem = ({ to }: { to: string }) => (
+  <ScrollLink className="nav_item" spy smooth offset={-80} to={to}>
+    <div className="nav_indicator" />
+    {to.charAt(0).toUpperCase() + to.slice(1)}
+  </ScrollLink>
+);
+
+const NavBar = () => {
   return (
     <div className="relative flex flex-col justify-between gap-10 p-4 lg:py-12 lg:fixed lg:h-[100dvh]">
       <div className="blur-animation">
@@ -10,32 +17,11 @@ const NavBar: any = () => {
         <h2 className="text-lg font-medium sm:text-xl">Web developer</h2>
         <h3>Creating innovative digital solutions</h3>
       </div>
-      <nav className="flex-col hidden gap-2 text-lg lg:flex text-primary mb-14 blur-animation">
-        <ScrollLink className="nav_item" spy smooth offset={-80} to="about">
-          <div className="nav_indicator" />
-          About
-        </ScrollLink>
-
-        <ScrollLink className="nav_item" spy smooth offset={-80} to="education">
-          <div className="nav_indicator" />
-          Education
-        </ScrollLink>
-
-        <ScrollLink
-          className="nav_item"
-          spy
-          smooth
-          offset={-80}
-          to="experience"
-        >
-          <div className="nav_indicator" />
-          Experience
-        </ScrollLink>
-
-        <ScrollLink className="nav_item" spy smooth offset={-80} to="projects">
-          <div className="nav_indicator" />
-          Projects
-        </ScrollLink>
+      <nav className="hidden flex-col gap-2 mb-14 text-lg lg:flex text-primary blur-animation">
+        <NavItem to="about" />
+        <NavItem to="education" />
+        <NavItem to="experience" />
+        <NavItem to="projects" />
       </nav>
       <SocialMedia />
     </div>
